7736376c0067a2d341777c07832b6f4a904b66d2
keisuke <ukei1234@yahoo.co.jp>
2022-01-16 22:04:26 +0900

    コメント追記

--- login.php
+++ login.php
@@ -3,21 +3,18 @@
 	require_once(__DIR__."/classes/Dao.php");
 	require_once(__DIR__."/constants.php");
 	session_start();
-	
+
+	//リクエストメソッドが「POST」ならば
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 		$msg = "";
+		//ユーザーIDが空ならば
 		if($_REQUEST['username'] == ""){
 			$msg = "ユーザーIDを入力してください。";
+		//パスワードが空ならば
 		}elseif($_REQUEST['password'] == ""){
 			$msg = "パスワードを入力してください。";
 		}else{
-			//$dsn = 'mysql:dbname='.Constants::DB_NAME.
-			//' host='.Constants::DB_HOST.
-			//' port='.Constants::DB_PORT;
-			//$user = Constants::DB_USER;
-			//$password = Constants::DB_PASS;
 
-		
 		try{
 			//$dbh = new PDO($dsn,$user,$password);
 			//PDO($dsn,$user,$password)はPHPがあらかじめ用意しているコンストラクタでデータベースへの接続の確立
@@ -29,13 +26,13 @@
 			//$stmt->bindParam(1,$_REQUEST['username']);
 			//$_REQUESTは$_POSTをGETでもPOSTでも見れるようにしたもの。
 			//上記のsql文の？を埋める
-			
+
 			//$stmt->execute();
 			//sqlを実行する。値は$stmtインスタンスの中に保管されている
-			
+
 			//$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 			//fetchAll(PDO::FETCH_ASSOC)でsqlの結果の取り出し
-			
+
 			//var_dump($users);
 			//検索結果がある場合は配列で取得できる
 			//ない場合はarray(0)が取得できる
@@ -44,28 +41,26 @@
 				exit;
 			}
 			*/
-			//if((!  empty($users)) && password_verify($_REQUEST['password'], $users[0]['password']) == true) {
-
+			//$usersに値が入っていて、かつ　パスワードがデータベース内で指定したユーザー名のものと一致したら、
 			if( $user['result'] && password_verify($_REQUEST['password'], $user['data']['password']) == true) {
-				//$usersに値が入っていて、かつ　パスワードがデータベース内で指定したユーザー名のものと一致したら、
-				
+
+				//セッションIDを再生成する
 				session_regenerate_id(TRUE);
 				$_SESSION['login'] = $_REQUEST['username'];
-				
+				//TOPページに遷移する。
 				header ('Location:'.Constants::TOP_URL);
-				//上記ページに遷移する。
 				exit;
 			}else{
 				$msg = "IDまたはパスワードが違います。";
 			}
-			
+
 			/*foreach($dbh->query($sql) as $row) {
 				print($row['user_id']);
 				print($row['password'].'<br>');
 			}
 			*/
+		//phpではない外部のアプリと連携するときはtry catchでエラーが起きた時の動きを定義
 		}catch (PDOException $e) {
-			//phpではない外部のアプリと連携するときはtry catchでエラーが起きた時の動きを定義した方が良い
 			print('Error:'.$e->getMessage());
 			die();
 			}
@@ -84,7 +79,7 @@
 		<form method="post">
 			<center>
 				<h1>ログイン</h1>
-			
+
 			</center>
 			<?php
 				if ($msg != ""){
@@ -92,7 +87,7 @@
 				}
 				?>
 				<!-$msgの値が空でなければ、$msgを出力する。->
-				
+
 			<div>
 				<center>
 					<div>
@@ -102,8 +97,8 @@
 					<div>
 						<input type="password" name="password" placeholder="Password">
 					</div>
-					
-					
+
+
 				</center>
 			</div>
 			<center>
