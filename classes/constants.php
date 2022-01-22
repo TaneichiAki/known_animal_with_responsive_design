e3382b22ff486d327a58bea44b44a498bafe342f
keisuke <ukei1234@yahoo.co.jp>
2022-01-22 23:13:10 +0900

    パス修正

--- classes/constants.php
+++ classes/constants.php
@@ -13,41 +13,41 @@ class Constants {
 	//ルートディレクトリのURL
 	const ROOT_URL = "/~testaki";
 	//login.phpのURL
-	const LOGIN_URL = Self::ROOT_URL."/aki/login.php";
+	const LOGIN_URL = Self::ROOT_URL."/known_animal/login/login.php";
 	//top.phpのURL
-	const TOP_URL = Self::ROOT_URL."/aki/top.php";
+	const TOP_URL = Self::ROOT_URL."/known_animal/top/top.php";
 	//delete.phpのURL
-	const DELETE_URL = Self::ROOT_URL."/aki/delete.php";
+	const DELETE_URL = Self::ROOT_URL."/known_animal/delete/delete.php";
 	//delete2.phpのURL
-	const DELETE2_URL = Self::ROOT_URL."/aki/delete2.php";
+	const DELETE2_URL = Self::ROOT_URL."/known_animal/delete/delete2.php";
 	//edit.phpのURL
-	const EDIT_URL = Self::ROOT_URL."/aki/edit.php";
+	const EDIT_URL = Self::ROOT_URL."/known_animal/animal_edit/edit.php";
 	//entry.phpのURL
-	const ENTRY_URL = Self::ROOT_URL."/aki/entry.php";
+	const ENTRY_URL = Self::ROOT_URL."/known_animal/animal_entry/entry.php";
 	//pass_change.phpのURL
-	const PASS_CHANGE_URL = Self::ROOT_URL."/aki/pass_change.php";
+	const PASS_CHANGE_URL = Self::ROOT_URL."/known_animal/pass_change/pass_change.php";
 	//pass_change_done.phpのURL
-	const PASS_CHANGE_DONE_URL = Self::ROOT_URL."/aki/pass_change_done.php";
+	const PASS_CHANGE_DONE_URL = Self::ROOT_URL."/known_animal/pass_change/pass_change_done.php";
 	//user_edit.phpのURL
-	const USER_EDIT_URL = Self::ROOT_URL."/aki/user_edit.php";
+	const USER_EDIT_URL = Self::ROOT_URL."/known_animal/user_edit/user_edit.php";
 	//last_name_edit.phpのURL
-	const LAST_NAME_EDIT_URL = Self::ROOT_URL."/aki/last_name_edit.php";
+	const LAST_NAME_EDIT_URL = Self::ROOT_URL."/known_animal/user_edit/last_name_edit.php";
 	//first_name_edit.phpのURL
-	const FIRST_NAME_EDIT_URL = Self::ROOT_URL."/aki/first_name_edit.php";
+	const FIRST_NAME_EDIT_URL = Self::ROOT_URL."/known_animal/user_edit/first_name_edit.php";
 	//department_edit.phpのURL
-	const DEPARTMENT_EDIT_URL = Self::ROOT_URL."/aki/department_edit.php";
+	const DEPARTMENT_EDIT_URL = Self::ROOT_URL."/known_animal/user_edit/department_edit.php";
 	//post_edit.phpのURL
-	const POST_EDIT_URL = Self::ROOT_URL."/aki/post_edit.php";
+	const POST_EDIT_URL = Self::ROOT_URL."/known_animal/user_edit/post_edit.php";
 	//birth_edit.phpのURL
-	const BIRTH_EDIT_URL = Self::ROOT_URL."/aki/birth_edit.php";
+	const BIRTH_EDIT_URL = Self::ROOT_URL."/known_animal/user_edit/birth_edit.php";
 	//mail_edit.phpのURL
-	const MAIL_EDIT_URL = Self::ROOT_URL."/aki/mail_edit.php";
+	const MAIL_EDIT_URL = Self::ROOT_URL."/known_animal/user_edit/mail_edit.php";
 	//new_account_done.phpのURL
-	const NEW_ACCOUNT_DONE_URL = Self::ROOT_URL."/aki/new_account_done.php";
+	const NEW_ACCOUNT_DONE_URL = Self::ROOT_URL."/known_animal/new_account/new_account_done.php";
 	//new_account.phpのURL
-	const NEW_ACCOUNT_URL = Self::ROOT_URL."/aki/new_account.php";
+	const NEW_ACCOUNT_URL = Self::ROOT_URL."/known_animal/new_account/new_account.php";
 	//pass_forget.phpのURL
-	const PASS_FORGET_URL = Self::ROOT_URL."/aki/pass_forget.php";
+	const PASS_FORGET_URL = Self::ROOT_URL."/known_animal/pass_forget/pass_forget.php";
 	//サーバー上の動物の写真の登録フォルダ
 	const ANIMAL_PHOTO_SERVER = "/home/testaki/animal_photo/";
 }
