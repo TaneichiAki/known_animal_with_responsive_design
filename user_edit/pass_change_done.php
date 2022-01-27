d0cf5c9326a27111aafd88dd92a8ff411b31513d
aki.taneichi <aki.0801.plum_wine@outlook.jp>
2022-01-27 22:15:16 +0900

    修正

--- user_edit/pass_change_done.php
+++ user_edit/pass_change_done.php
@@ -1,23 +1,24 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="utf-8">
-</head>
-<body>
-	<?php
+<?php
 	require_once(__DIR__."/../classes/constants.php");
 	session_start();
 
+	//セッションIDがセットされていなかったらログインページに戻る
 	if(! isset($_SESSION['login'])){
-			header("Location:".Constants::LOGIN_URL);
-			exit();
-		}
-	?>
-<center>
-	<br>
-	<br>
-	<p>パスワードを変更しました！</p>
-		<button type="button" onclick="location.href='<?php echo Constants::TOP_URL?>'">トップページへ戻る</button>
-
-</center>
-</body>
+		header("Location:".Constants::LOGIN_URL);
+		exit();
+	}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<center>
+			<br>
+			<br>
+			<p>パスワードを変更しました！</p>
+			<button type="button" onclick="location.href='<?php echo Constants::TOP_URL?>'">トップページへ戻る</button>
+		</center>
+	</body>
+</html>
