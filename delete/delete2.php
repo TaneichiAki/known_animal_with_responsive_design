5aebddf2a45786464431bad397eb03093f9e1bb2
example <example@example.com>
2022-02-06 16:18:49 +0900

    リファクタリング

--- delete/delete2.php
+++ delete/delete2.php
@@ -1,29 +1,28 @@
+<?php
+	require_once(__DIR__."/../classes/constants.php");
+	session_start();
+	//セッションIDがセットされていなかったらログインページに戻る
+	if(! isset($_SESSION['login'])){
+		header('Location:'.Constants::LOGIN_URL);
+		exit();
+	}
+?>
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-<meta charset="utf-8">
-<title>削除完了</title>
-</head>
-<body>
-	<?php
-	require_once(__DIR__."/../classes/constants.php");
-		session_start();
-
-		if(! isset($_SESSION['login'])){
-				header('Location:'.Constants::LOGIN_URL);
-				exit();
-			}
-			//セッションIDがセットされていなかったらログインページに戻る
-	?>
-
-<center>
-	<h1>削除しました！</h1>
-	<script type="text/javascript">
-function refresh(){
-    window.opener.location.reload();
-    window.close();
-}
-</script>
-	<button type="button" onclick="refresh()">閉じる</button>
-</center>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title>削除完了</title>
+	</head>
+	<body>
+		<center>
+			<h1>削除しました！</h1>
+			<script type="text/javascript">
+				function refresh(){
+					window.opener.location.reload();
+					window.close();
+				}
+			</script>
+			<button type="button" onclick="refresh()">閉じる</button>
+		</center>
+	</body>
+</html>
