2165c3a45c288a5b1c5bc3db0283b25c669be08c
aki.taneichi <aki.0801.plum_wine@outlook.jp>
2022-04-09 22:34:56 +0900

    edit.phpのデザイン更新

--- animal_edit/edit.php
+++ animal_edit/edit.php
@@ -34,15 +34,16 @@
 		*/
 		protected function post(){
 			if($_REQUEST['family'] == ""){
-				return “何科か入力してください。“;
+				return “何科か入力してください。”;
 			}
 			if($_REQUEST['features'] == ""){
-				return “特徴を入力してください。“;
+				return “特徴を入力してください。”;
 			}
-			if($_REQUEST['date'] == ""){
-				return “知った日を入力してください。“;
+			if($_REQUEST['date('] == ""){
+				return “知った日を入力してください。”;
 			}
-			update();
+			$this->update();
+
 			//下記TOPページに遷移する。
 			header ('Location:'.Constants::TOP_URL);
 			exit;
@@ -63,42 +64,54 @@
 <html lang=“ja”>
 	<head>
 		<meta charset="utf-8">
+		<!-- Required meta tags -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Bootstrap CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 		<title>更新</title>
 	</head>
 	<body>
 		<div>
-			<form method="post" enctype="multipart/form-data">
-				<center>
-					<h1>データの更新</h1>
-					<?php
-					if ($msg != ""){
-						echo "<center><div>".$msg."</div></center>";
+			<form class="border offset-3 col-6 rounded bg-light mt-5" method="post" enctype="multipart/form-data">
+					<div class="offset-md-3 col-md-6 text-center mt-3">
+					<h2>動物データの更新</h2>
+					<h3><?php
+					if ($obj->msg != ""){
+						echo "<center><div>".$obj->msg."</div></center>";
 					}
-					?>
-					<div>
-						<div>
-							<?php echo "対象の動物：".$select_animal ?>
+					?></h3>
+				</div>
+					<div class="container-fluid">
+
+						<div class="offset-md-3 col-md-6 text-center mt-5">
+							<h4><?php echo "対象の動物：".$select_animal ?></h4>
 						</div>
-						<div>
-							<input type="family" name="family" placeholder="何科" value="<?php echo $select_family ?>" >
+						<div class="offset-md-3 col-md-6 form-inline">
+							<label for="family" class="control-label">何科</label>
+							<input type="family" class="form-control col-md-2" id="family" name="family" placeholder="何科" value="<?php echo $select_family ?>" >
 						</div>
-						<div>
-							<input type="features" name="features" placeholder="特徴" value="<?php echo $select_features ?>" >
+						<div class="offset-md-3 col-md-6">
+							<label for="features" class="control-label">特徴</label>
+							<input type="features" class="form-control" name="features" placeholder="特徴" value="<?php echo $select_features ?>" >
 						</div>
-						<div>
-							<input type="date" name="date" placeholder="知った日" value="<?php echo $select_date ?>" >
+						<div class="offset-md-3 col-md-6">
+							<label for="date" class="control-label">知った日</label>
+							<input type="date" class="form-control" name="date" placeholder="知った日" value="<?php echo $select_date ?>" >
 						</div>
-						<div>
-							<input type="file" name="image">
+						<div class="offset-md-3 col-md-6">
+							<label for="file" class="control-label">イメージ画像</label>
+							<input type="file" class="form-control" name="image">
 							<br>
-							<br>
 						</div>
+						<button class="offset-md-5 col-md-2 btn btn-primary btn-sm mt-2 mb-3" type="submit">更新</button>
+						<input type = "hidden" name = "update_animal"  value = "<?= $select_no ?>">
+						<br>
+						<button class="offset-md-5 col-md-2 btn btn-primary btn-sm mt-2 mb-3" type="button" onclick="location.href='<?php echo Constants::TOP_URL?>'">トップページへ戻る</button>
+
 					</div>
-					<input type="submit" value="更新">
-					<input type = "hidden" name = "update_animal"  value = "<?= $select_no ?>">
-					<br>
-					<button type="button" onclick="location.href='<?php echo Constants::TOP_URL?>'">トップページへ戻る</button>
-				</center>
 			</form>
 		</div>
 	</body>
