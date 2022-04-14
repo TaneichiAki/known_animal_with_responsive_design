2f9eb3016876eca2ef51c2fb668038ed17f14e54
aki.taneichi <aki.0801.plum_wine@outlook.jp>
2022-04-15 00:36:06 +0900

    更新

--- animal_entry/entry.php
+++ animal_entry/entry.php
@@ -2,6 +2,11 @@
 	require_once(__DIR__."/../classes/Dao.php");
 	require_once(__DIR__."/../classes/constants.php");
 	require_once(__DIR__."/../classes/knownAnimalBase.php");
+
+	class Entry extends KnownAnimalBase{
+		
+	}
+
 	/**
 	*グローバル変数定義
 	*/
