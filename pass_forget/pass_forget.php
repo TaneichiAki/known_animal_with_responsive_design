4090367cb9f015c93f670dadffcbeb474cd1c8d7
aki.taneichi <aki.0801.plum_wine@outlook.jp>
2022-04-29 12:00:11 +0900

    パスワード再設定画面更新

--- pass_forget/pass_forget.php
+++ pass_forget/pass_forget.php
@@ -98,30 +98,34 @@
 <html lang="ja">
 	<head>
 		<meta charset="utf-8">
+		<!-- Required meta tags -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Bootstrap CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 		<title>パスワード再設定画面</title>
 	</head>
 	<body>
-		<form method="post">
-		<center>
-			<h1>パスワード再設定</h1>
+		<div class="offset-md-4 col-md-4 text-center mt-3 mb-3">
+		<form class="border rounded bg-light mt-5 mb-5" method="post">
+			<h4 class="mt-3">パスワード再設定</h4>
 				<?php
 					if ($msg != ""){
 						echo "<center><div>".$msg."</div></center>";
 					}
 				?>
 				<br>
+			<div class="container-fluid">
 			<p>known_animalシステムアカウントに関連づけられているEメールアドレスを入力してください。</p>
-			<input type='email' name='mail'>
-			<br>
-			<br>
-			<input type='submit' value='パスワードを再設定'>
-			<br>
-			<br>
-			<div style="padding: 10px 3px; margin: 5px 500px; border: 1px dashed #333333; border-radius: 10px;">
-				<p>通常通りログインする方はこちら</p>
-				<button type="button" onclick="location.href='<?php echo Constants::LOGIN_URL?>'"> 今すぐログイン</button>
+			<div class="offset-md-2 col-md-8">
+			<input type='email' class="form-control" name='mail'>
+			<button class="col-md-8 btn btn-primary btn-sm mt-4 mb-4" type="submit">パスワードを再設定</button>
 			</div>
-		</center>
+			</div>
 		</form>
+		<a href="<?php echo Constants::LOGIN_URL?>" class="link-primary text-decoration-none">ログイン画面へ戻る</a>
+		</div>
 	</body>
 </html>
